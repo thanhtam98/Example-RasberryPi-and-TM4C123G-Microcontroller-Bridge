af26310209650acf61f0bd4a528486c2fd0271b7
Thanh Tam Nguyen <1613058@hcmut.edu.vn>
2019-06-27 15:24:05 +0700

    edit data frame and add data_handler function when complete receive data from serial

--- Rasb/test_afproto.py
+++ Rasb/test_afproto.py
@@ -2,44 +2,44 @@ import serial
 import struct
 from afproto import afproto_frame_data
 from afproto import afproto_get_data
-
+import threading 
 import time
 
 START_BIT = '\x12'
-STOP_BIT = '\x13'
+STOP_BIT  = '\x13'
 
-def serial_write_data(Len,ID,FT,CMDnumber,data):
-    len_data = len(data)
-    print(len_data)
-    frame = struct.pack('>BBB'+'H'+'B'*len_data,Len,ID,FT,CMDnumber,*data)
+def serial_write_data(get_frame=[], *args):
+    len_data = len(get_frame)
+    # print(len_data)
+    frame = struct.pack('B'*len_data,*get_frame)
     encoded_frame = afproto_frame_data(frame)
     ser.write(encoded_frame)
-
+def serial_data_handler(frame=[], *args):
+    
+    print(frame)
+    
 def serial_read_data():
-    IS_Started = 0 
-    frame_recevied = ""
-    while (True):
-        raw_data = ser.read()
-        if raw_data == START_BIT:
-            print("after start bit ")
-            IS_Started=1
-        if IS_Started == 1 :
-            frame_recevied += raw_data
-            print(raw_data)
-        if raw_data == STOP_BIT:
-            print("stop")
-            break
-    data_decode,raw_frame_data = afproto_get_data(frame_recevied)
+    while(True):
+        IS_Read    = 0
+        IS_Started = 0 
+        frame_recevied = ""
+        while (True):
+            raw_data = ser.read()
+            if raw_data == START_BIT:
+                print("after start bit ")
+                IS_Started=1
+            if IS_Started == 1 :
+                frame_recevied += raw_data
+                print(raw_data)
+            if raw_data == STOP_BIT:
+                print("stop")
+                break
+        data_decode,raw_frame_data = afproto_get_data(frame_recevied)
 
-    if (data_decode != None):
-        print("crc matched")
-        Len     = data_decode[0]
-        ID      = data_decode[1]
-        FT      = data_decode[2]
-        CMDnumber = data_decode [3:5]
-        data    =  data_decode[5:]
-        print(Len,ID,FT,CMDnumber,data)
-        return Len,ID,FT,CMDnumber,data
+        if (data_decode != None):
+            print("crc matched and jump into handler")
+            serial_data_handler(data_decode)
+        
 ser = serial.Serial(port='/dev/ttyS0',
                baudrate = 115200,
                parity=serial.PARITY_NONE,
@@ -48,9 +48,11 @@ ser = serial.Serial(port='/dev/ttyS0',
                timeout=1)
 print("Serial Initial Successful")
 
+serial_data_thread = threading.Thread(target=serial_read_data)
+serial_data_thread.start()
+
 while True:
 
-
-    # serial_write_data(0x05,0x88,0x03,0x555,[0x56,0x68,0x69,0x80])
-    # serial_read_data()
-    # time.sleep(1)
+    payload= [0x05,0x88,0x03,0x22,0x33,0x56,0x68,0x69,0x82]
+    serial_write_data(payload)
+    time.sleep(1)
